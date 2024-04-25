c4b693e93111d21b045931f70c3e0b529160c514
ashish081099 <82055417+ashish081099@users.noreply.github.com>
2024-04-26 01:25:29 +0530

    Add files via upload

---
+++ Plot_AQI.py
@@ -0,0 +1,136 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def avg_data_2018():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv("/DS Projects/Air_quality_index/AQI/aqi2018.csv",chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
+                    temp=float(i)
+                    add_var=add_var+temp
+        avg=add_var/24
+        temp_i=temp_i+1
+
+        average.append(avg)
+    return average
+
+def avg_data_2019():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv("/DS Projects/Air_quality_index/AQI/aqi2019.csv",chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
+                    temp=float(i)
+                    add_var=add_var+temp
+        avg=add_var/24
+        temp_i=temp_i+1
+
+        average.append(avg)
+    return average
+
+def avg_data_2020():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv("/DS Projects/Air_quality_index/AQI/aqi2020.csv",chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
+                    temp=float(i)
+                    add_var=add_var+temp
+        avg=add_var/24
+        temp_i=temp_i+1
+
+        average.append(avg)
+    return average
+
+def avg_data_2021():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv("/DS Projects/Air_quality_index/AQI/aqi2021.csv",chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
+                    temp=float(i)
+                    add_var=add_var+temp
+        avg=add_var/24
+        temp_i=temp_i+1
+        average.append(avg)
+    return average
+
+def avg_data_2022():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv("/DS Projects/Air_quality_index/AQI/aqi2022.csv",chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index, row in df.iterrows():
+            data.append(row['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='InVld':
+                    temp=float(i)
+                    add_var=add_var+temp
+        avg=add_var/24
+        temp_i=temp_i+1
+
+        average.append(avg)
+    return average
+
+if __name__=="__main__":
+    lst2018 = avg_data_2018()
+    lst2019 = avg_data_2019()
+    lst2020 = avg_data_2020()
+    lst2021 = avg_data_2021()
+    lst2022 = avg_data_2022()
+
+    plt.plot(range(0,365),lst2018, label='2018 Data')
+    plt.plot(range(0,365),lst2019, label='2019 Data')
+    #plt.plot(range(0,364),lst2020, label='2020 Data')
+    plt.plot(range(0,365),lst2021, label='2021 Data')
+    #plt.plot(range(0,364),lst2022, label='2022 Data')
+    plt.xlabel('Day')
+    plt.ylabel('PM 2.5')
+    plt.legend(loc='upper right')
+    plt.show()
